b571c3d562492e1c04e2f85dcf986af55b3794f8
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-05-31 23:12:54 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -3,13 +3,17 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         StringBuilder sb = new StringBuilder();
         
-        for(int i=0; i<s; i++) {
-        	sb.append(Character.toString(my_string.charAt(i)));
-        }
+        // for(int i=0; i<s; i++) {
+        // 	sb.append(Character.toString(my_string.charAt(i)));
+        // }
+        // sb.append(overwrite_string);
+        // for(int j=s+overwrite_string.length(); j<my_string.length(); j++) {
+        // 	sb.append(Character.toString(my_string.charAt(j)));
+        // }
+        sb.append(my_string.substring(0,s));
         sb.append(overwrite_string);
-        for(int j=s+overwrite_string.length(); j<my_string.length(); j++) {
-        	sb.append(Character.toString(my_string.charAt(j)));
-        }
+        sb.append(my_string.substring(s+overwrite_string.length(), my_string.length()));
+        
         
         return sb.toString();
     }
