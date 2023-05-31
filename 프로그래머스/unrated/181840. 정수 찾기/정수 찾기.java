d6e6d8162decf2b44a8b36ef90cc9c1d51c76fe2
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-05-31 23:32:06 +0900

    [unrated] Title: 정수 찾기, Time: 1.13 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -1,9 +1,10 @@
+import java.util.stream.*;
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
-        for(int num: num_list) {
-            if(num==n) answer=1;
-        }
-        return answer;
+        // int answer = 0;
+        // for(int num: num_list) {
+        //     if(num==n) answer=1;
+        // }
+        return IntStream.of(num_list).anyMatch(num -> num == n)? 1: 0;
     }
 }
