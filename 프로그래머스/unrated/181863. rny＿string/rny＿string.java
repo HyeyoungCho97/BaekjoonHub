32f33f90c0a0494c7377acd237f3876cffa68d12
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-05-31 23:56:04 +0900

    [unrated] Title: rny_string, Time: 0.08 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.java
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public String solution(String rny_string) {
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
         
-        for(int i=0; i<rny_string.length(); i++){
-            if ('m'==rny_string.toCharArray()[i]) {
-            	sb.append("rn");
-            }else sb.append(rny_string.toCharArray()[i]);
-        }
+//         for(int i=0; i<rny_string.length(); i++){
+//             if ('m'==rny_string.toCharArray()[i]) {
+//             	sb.append("rn");
+//             }else sb.append(rny_string.toCharArray()[i]);
+//         }
        
-        return sb.toString();
+        return rny_string.replaceAll("m", "rn");
     }
 }
