5f7558806d693b0d401a564aa65f9bce6cc16311
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-06-14 11:15:12 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a%2==1 && b%2==1) return a*a+b*b;
+        else if(a%2==1 || b%2==1) return 2*(a+b);
+        else if(a%2!=1 && b%2!=1) return (a-b >0)? a-b: (a-b)*(-1);
+        
+        return -1;
+    }
+}
