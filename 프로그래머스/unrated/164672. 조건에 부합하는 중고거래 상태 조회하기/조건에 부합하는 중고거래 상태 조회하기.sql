008468c8a483e61fa3fbff4b7e812ea33a9a86d6
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-06-30 23:18:09 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+SELECT  board_id, 
+        writer_id, 
+        title, 
+        price, 
+        CASE
+            WHEN status LIKE 'SALE' THEN '판매중'
+            WHEN status LIKE 'RESERVED' THEN '예약중'
+            WHEN status LIKE 'DONE' THEN '거래완료'
+            ELSE 'NONE'
+        END
+FROM    used_goods_board
+WHERE   created_date LIKE '2022-10-05'
+ORDER BY    board_id DESC
