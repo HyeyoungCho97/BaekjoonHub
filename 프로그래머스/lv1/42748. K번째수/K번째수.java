590461cf2183ffa4dc02d14bc8d216c398d25e30
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-07-20 18:41:34 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -3,12 +3,15 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int aIdx=0;
+        //command만큼 반복
         for(int[] c: commands){
-            int[] subArr = new int[c[1]-c[0]+1];
-            int idx=0;
-            for(int i=c[0]-1; i<c[1];i++) subArr[idx++] = array[i];
-            Arrays.sort(subArr);
-            answer[aIdx++] = subArr[c[2]-1];
+            int[] temp = new int[c[1]-c[0]+1];
+            int tIdx = 0;
+            for(int i=c[0]-1; i<c[1]; i++){
+                temp[tIdx++] = array[i];
+            }
+            Arrays.sort(temp);
+            answer[aIdx++] = temp[c[2]-1];
         }
         
         return answer;
