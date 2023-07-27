d3b2522e5f9939988093a65f0fcf9088700d20b4
HyeyoungCho <48588777+HyeyoungCho97@users.noreply.github.com>
2023-07-27 19:44:50 +0900

    [Gold V] Title: 토마토, Time: 688 ms, Memory: 122036 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,69 +1,83 @@
-import java.io.*;
-import java.util.*;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
 	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M= Integer.parseInt(st.nextToken());
-		int N= Integer.parseInt(st.nextToken());
-		int[][] box= new int[N][M];
-		Queue<Tomato> q= new LinkedList<Tomato>();
-		int dx[] = {1, 0, -1, 0};
-		int dy[] = {0, 1, 0, -1};
-		int date = 0;
-		//box 초기화 및 q에 넣기
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++) {
-				box[i][j]=Integer.parseInt(st.nextToken());
-				if(box[i][j]==1) q.offer(new Tomato(i, j, 0));
-			}
-		}
-		while(!q.isEmpty()) {
-			Tomato tomato= q.poll();
-			date = (tomato.day > date)? tomato.day: date;
-			//System.out.println(tomato.locX+" "+tomato.locY);
-			for(int i=0; i<4; i++) {
-				int toX = tomato.locX+dx[i];
-				int toY = tomato.locY+dy[i];
-				//System.out.println(toX+" "+toY);
-				if(toX<0 || toX>=N || toY<0 || toY>=M ) continue;
-				if(box[toX][toY]==-1)continue;
-				
-				if(box[toX][toY]==0) {
-					//System.out.println(toX+" "+toY);
-					q.offer(new Tomato(toX, toY, tomato.day+1));
-					box[toX][toY]=1;
-				}	
-			}	
-		}
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(box[i][j] ==0) {
-					date=-1;
-					break;
-				}
-			}
-		}
-		
-		System.out.println(date);
-		
-		
-	}
+   
+    public static void main(String[] args) throws IOException {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	int N= Integer.parseInt(st.nextToken());
+    	int M= Integer.parseInt(st.nextToken());
+    	int[][] box = new int[M][N];
+    	
+    	Queue<Tomato> q = new LinkedList<>();
+    	int[] dx= {0, 0, 1, -1};
+    	int[] dy= {1, -1, 0, 0};
+    	
+    	//box 초기화
+    	for(int i=0; i<M; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		for(int j=0; j<N; j++) {
+    			box[i][j] = Integer.parseInt(st.nextToken());
+    			//익은 토마토 표시
+    			if (box[i][j]==1) q.offer(new Tomato(i, j, 0));
+    		}
+    	}
+    	
+    	int maxDate=0;
+    	//BFS
+    	while(!q.isEmpty()) {
+    		
+    		Tomato now = q.poll();
+    		
+    		for(int i=0; i<4; i++) {
+    			int nextX = now.x+dx[i];
+    			int nextY = now.y+dy[i];
+    			
+    			if(nextX<0 || nextX>=M || nextY<0 || nextY>=N) continue;
+    			if(box[nextX][nextY]==-1)continue;
+    			if(box[nextX][nextY]==1)continue;
+    			
+    			box[nextX][nextY]= 1;
+    			q.offer(new Tomato(nextX, nextY, now.day+1)); 
+    		}
+    		maxDate = (maxDate<now.day)? now.day: maxDate;
+    	}
+    	
 
+    	//상자 확인
+    	for(int i=0; i<M; i++) {
+    		for(int j=0; j<N; j++) {
+    			if(box[i][j]==0) {
+
+    				System.out.println("-1");
+    				return;
+    			}
+    		}
+    	}
+    	
+    	System.out.println(maxDate);
+    	
+    }
 }
+
 class Tomato{
-	int locX;
-	int locY;
+	
+	int x;
+	int y;
 	int day;
 	
-	Tomato(int locX, int locY, int day) {
-		this.locX= locX;
-		this.locY= locY;
-		this.day= day;
+	Tomato(int x, int y, int day){
+		this.x= x;
+		this.y= y;
+		this.day=day;
 	}
 }
