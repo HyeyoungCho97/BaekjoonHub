c423dc4729bbd37d74ec4fbd93d283f76491a762
cho_doit <48588777+HyeyoungCho97@users.noreply.github.com>
2025-01-22 00:10:07 +0900

    [level 0] Title: x 사이의 개수, Time: 2.65 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,16 +1,3 @@
 def solution(myString):
-    answer = []
-    num = 0
-    
-    for str in myString :
-        if str!='x' :
-            num += 1
-        else :
-            answer.append(num)
-            num = 0
-            
-    answer.append(num)
-    
         
-        
-    return answer
+    return [len(str) for str in myString.split('x')]
