d6a1fcfc96626959af1a1df5db86415ff3d30205
cho_doit <48588777+HyeyoungCho97@users.noreply.github.com>
2025-01-21 23:54:40 +0900

    [level 0] Title: 코드 처리하기, Time: 38.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -2,18 +2,12 @@ def solution(code):
     answer = ''
     mode = 0
     
-    for idx in range(0, len(code)):
+    for idx in range(len(code)):
         
         if code[idx]=='1' :
-            mode = (mode+1)%2
-            continue
-            
-        answer = mode0(idx, code[idx], answer) if mode==0 else mode1(idx, code[idx], answer)
-
+            mode = 1-mode
+        
+        else :
+            answer = answer+code[idx] if mode == idx%2 else answer
+        
     return answer if answer!='' else 'EMPTY'
-
-def mode0(idx, value, answer):
-    return answer+value if value!=1 and idx%2==0 else answer
-
-def mode1(idx, value, answer):
-    return answer+value if value!=1 and idx%2==1 else answer
