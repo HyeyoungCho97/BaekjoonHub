affb3657eadbb191bce736d131e2d94478b86594
cho_doit <48588777+HyeyoungCho97@users.noreply.github.com>
2025-01-31 00:01:18 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,10 +1,4 @@
 def solution(a, d, included):
     
-    answer = a if included[0] else 0
-    num = a
-    
-    for x in included[1:]:
-        num = num+d
-        answer = answer+num if x else answer
-        
-    return answer
+    # for i, v in enumerate(array) => index, value     
+    return sum(a + i * d for i, v in enumerate(included) if v)
